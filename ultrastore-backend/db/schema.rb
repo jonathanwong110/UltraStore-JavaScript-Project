48268be16c1566a96dd95637478bb62d0f641220
jon <jonathan.wong110@gmail.com>
2019-12-04 12:54:41 -0500

    created new files/folders in the src folder and started filling out the productsadapter.js

--- ultrastore-backend/db/schema.rb
+++ ultrastore-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_170638) do
+ActiveRecord::Schema.define(version: 2019_12_04_173721) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2019_12_04_170638) do
     t.string "description"
     t.string "category"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
