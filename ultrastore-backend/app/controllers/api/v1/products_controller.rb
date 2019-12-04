ac98123ac1160b309dc5ad896a592a19a7ea8b73
jon <jonathan.wong110@gmail.com>
2019-12-04 12:04:06 -0500

    built out basic products controller

---
+++ ultrastore-backend/app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,39 @@
+class Api::V1::ProductsController < ApplicationController
+    
+    def index
+        @products = Product.all
+        render json: @products, status: 200
+    end
+
+    def show
+        @product = Product.find(params[:id])
+        render json: @product, status: 200
+    end
+
+    def create
+        @product = Product.create(product_params)
+        render json: @product, status: 200
+    end
+
+    def update
+        @product = Product.find(params[:id])
+        @product.update(product_params)
+        render json: @product, status: 200
+    end
+
+    def index
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+        @product.delete
+        render json: {productId: @product.id}
+    end
+    
+    private
+    
+    def product_params
+        params.require(:product).permit(:title, :price, :description, :category, :user_id)
+    end
+
+end
